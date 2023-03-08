e27a065d67a8728dc1e026f974e80e1da16f7f2a
Mark Olah <114992682+OlahMark19@users.noreply.github.com>
2023-03-08 13:44:57 +0200

    Add files via upload

---
+++ Bubble Sort/Bubble Sort/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bubble_Sort
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] x = { 9, 1, 8, 2, 7, 3, 6, 4, 5 };
+            int temp = 0;
+            for (int i = 0; i < x.Length; i++)
+            {
+                for (int j =i + 1; j < x.Length; j++)
+                {
+                    if (x[j - 1] > x[j])
+                    {
+                        temp = x[j - 1];
+                        x[j - 1] = x[j];
+                        x[j] = temp;
+                    }
+                }
+            }
+            for (int i = 0; i < x.Length; i++)
+            {
+                Console.WriteLine(x[i]);
+            }
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Selection Sort/Selection Sort/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Selection_Sort
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] x = { 9, 1, 8, 2, 7, 3, 6, 4, 5 };
+            int temp = 0;
+            int min = 0;
+            int pozMin = 0;
+            for (int i = 0; i < x.Length; i++)
+            {
+                min = x[i];
+                for (int j = i; j < x.Length; j++) 
+                {
+                    if (min > x[j])
+                    {
+                        min = x[j];
+                        pozMin = j;
+                    }
+                }
+                temp = x[i];
+                x[i] = min; 
+                x[pozMin] = temp;
+            }
+            for (int i = 0; i < x.Length; i++)
+            {
+                Console.WriteLine(x[i]);
+            }
+           
+            Console.ReadKey();
+        }
+    }
+}
